8907b998211e898f8be71a13f73db13bafe2e3b9
WazeerAhmed18 <148325463+WazeerAhmed18@users.noreply.github.com>
2024-02-21 21:12:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+var sidenav = document.querySelector(".side-navbar")
+
+function show()
+{
+    sidenav.style.left="0"
+}
+function cancel()
+{
+    sidenav.style.left="-60%"
+}
+
+
+var productContainer = document.getElementById("product")
+var search = document.getElementById("search")
+var productlist = productContainer.querySelectorAll("div")
+
+search.addEventListener("keyup", function(){
+    var enteredvalue = event.target.value.toUpperCase()
+
+    for(count=0; count<productlist.length; count=count+1)
+    {
+        var productname = productlist[count].querySelector("p").textContent
+
+        if(productname.toUpperCase().indexOf(enteredvalue) < 0)
+        {
+            productlist[count].style.display="none"
+        }
+        else{
+            productlist[count].style.display="block"
+        }
+    }
+
+})
